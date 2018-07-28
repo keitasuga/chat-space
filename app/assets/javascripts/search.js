f11890fd4e2be214e90bbbee76ece6634a9f8efd
keitasuga <keita.sugahara@gmail.com>
2018-07-28 21:21:34 +0900

    Add incremental search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,7 +5,26 @@ $(function(){
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name=${user.name}>追加</a>
               </div>`
     $("#user-search-result").append(html);
+    console.log(user)
   }
+
+  function appendNoUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user }
+              </div>`
+    $("#user-search-result").append(html);
+  }
+
+  function addToGroup(userId, userName){
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value= ${userId}>
+                <p class='chat-group-user__name'> ${userName}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    $("#chat-group-users").append(html);
+    console.log()
+  }
+
   $('#user-search-field').on('keyup', function() {
     var input = $(this).val();
     console.log(input);
@@ -18,12 +37,28 @@ $(function(){
     })
     .done(function(users) {
       $("#user-search-result").empty();
-      users.forEach(function(user){
-        appendUser(user);
-      });
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser('該当するユーザーはいません。');
+      }
     })
     .fail(function() {
-      alert('メンバー検索に失敗しました');
+      alert('ユーザー検索に失敗しました');
     })
+    $(document).on('click', '.user-search-add', function () {
+      console.log(this)
+      var userId = $(this).attr("data-user-id")
+      var userName = $(this).attr("data-user-name")
+      console.log(userName)
+      $(this).parent(".chat-group-user").empty();
+      addToGroup(userId, userName);
+    });
+    $(document).on('click', '.user-search-remove', function () {
+      $(this).parent(".chat-group-user").empty();
+    });
   });
 });
