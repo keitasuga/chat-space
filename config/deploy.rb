ab5edf881a508adf7801356b3dcf46a33b0b80d5
keitasuga <keita.sugahara@gmail.com>
2018-08-04 19:52:33 +0900

    Edit capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:keitasuga/chat-space.git'
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/sugaharakeita/.ssh/keypair.pem']  
+                  keys: ['/Users/sugaharakeita/.ssh/keypair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
